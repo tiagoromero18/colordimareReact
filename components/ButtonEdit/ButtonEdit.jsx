97fa1a8b50365bbd3758f7fea7f059c199953c69
camilarnahuel <71852268+camilarnahuel@users.noreply.github.com>
2023-11-29 00:02:16 -0500

    Add files via upload

--- components/ButtonEdit/ButtonEdit.jsx
+++ components/ButtonEdit/ButtonEdit.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './ButtonEdit.css';
 
 const EditButton = ({ to, text }) => {
   return (
     <Link to="/addNewProduct">
-      <button className="btn" type="button" style={{ backgroundColor: '#FEF5EF' }}>
+      <div id = 'b'>
+        <button className="btn" type="button" style={{ backgroundColor: '#FEF5EF' }}>
             Agregar Producto
           </button>
+      </div>
     </Link>
   );
 };
